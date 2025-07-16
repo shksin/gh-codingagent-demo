5759766e9c9e65a60dbca03a30ed90f1d2a869a5
shksin <deepshikha16@gmail.com>
2025-07-16 12:20:20 +1000

    revert credit card changes

--- creditcard-react-app/app/credit-card/credit-card-service.ts
+++ creditcard-react-app/app/credit-card/credit-card-service.ts
@@ -1,12 +1,10 @@
 const MAX_DIGITS = 16;
 const EXPIRE_YEAR_LENGTH = 3;
 
-// return random 16 digit credit card number
+
 export const getCreditCardNumber = () => {
-  const digits = Array.from({ length: MAX_DIGITS }, () => Math.floor(Math.random() * 10));
-  return digits.join("");
+  return "1234 5678 9012 3456";
 };
-  //return "1234 5678 9012 3456";
 
 
 export function generateExpiryDate() {
